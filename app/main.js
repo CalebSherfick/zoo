9c06defed8e8f945c5cf330d51f9b9943b9a1d41
Caleb Sherfick <calebsherfick@gmail.com>
2019-02-11 13:27:27 -0700

    imported and created instances

---
+++ app/main.js
@@ -0,0 +1,55 @@
+import Alligator from "./models/alligator.js";
+import Bear from "./models/bear";
+import Crab from "./models/crab";
+import Donkey from "./models/donkey";
+import Elephant from "./models/elephant";
+import Flamingo from "./models/flamingo";
+import Gorilla from "./models/gorilla";
+import Horse from "./models/horse";
+import Iguana from "./models/iguana";
+import Jaguar from "./models/jaguar";
+import Koala from "./models/koala";
+import Lynx from "./models/lynx";
+import Meerkat from "./models/meerkat";
+import Narwhal from "./models/narwhal";
+import Ostrich from "./models/ostrich";
+import Penguin from "./models/penguin";
+import Quail from "./models/quail";
+import Raccoon from "./models/raccoon";
+import Snake from "./models/snake";
+import Turtle from "./models/turtle";
+import Urchin from "./models/urchin";
+import Vulture from "./models/vulture";
+import Whale from "./models/whale";
+import Xerux from "./models/xerus";
+import Yak from "./models/yak";
+import Zebra from "./models/zebra"
+
+
+let alex = new Alligator('Alex', 4, true)
+let barry = new Bear('Barry', 4, true)
+let chris = new Crab('Chris', 6, false)
+let darryl = new Donkey('Darryl', 4, true)
+let ellie = new Elephant('Ellie', 4, true)
+let frank = new Flamingo('Frank', 2, true)
+let george = new Gorilla('George', 2, true)
+let harry = new Horse('Harry', 2, true)
+let izzy = new Iguana('Izzy', 2, true)
+let james = new Jaguar('James', 2, true)
+let karen = new Koala('Karen', 2, true)
+let laurel = new Lynx('Laurel', 2, true)
+let mary = new Meerkat('Mary', 2, true)
+let narth = new Narwhal('Narth', 2, true)
+let olly = new Ostrich('Olly', 2, true)
+let patty = new Penguin('Patty', 2, true)
+let quinn = new Quail('Quinn', 2, true)
+let rich = new Raccoon('Rich', 2, true)
+let sally = new Snake('Sally', 2, true)
+let trevor = new Turtle('Trevor', 2, true)
+let uni = new Urchin('Uni', 2, true)
+let vivian = new Vulture('Vivian', 2, true)
+let wanda = new Whale('Wanda', 2, true)
+let xavier = new Xerus('Xavier', 2, true)
+let yanny = new Yak('Yanny', 2, true)
+let zack = new Zebra('Zack', 2, true)
+
